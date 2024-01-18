6ca955ad3ac6381b5075fc03b56f4f4a4742b29b
Ilija Boshkov <ilija@codechem.com>
2024-01-18 13:57:38 +0100

    Fix image display

--- frontend/Program.cs
+++ frontend/Program.cs
@@ -57,14 +57,15 @@ app.MapRazorPages();
 app.MapGet("/events/{eventSlug}", async (string eventSlug) => {
     var res = await GetStrapiEntry($"events/?filters[slug][$eq]={eventSlug}");
     var evt = res?["data"]?[0];
-    var url = evt?["promo"]?["data"]?["url"]?.ToString();
+    var url = evt?["promo"]?["url"]?.ToString();
     var title = evt?["title"].ToString();
+    var calendarUrl = evt?["calendarUrl"]?.ToString();
     var fullUrl = url is null ? null : $"{Constants.StrapiUrlBase}{url.Substring(1)}";
     var hasStart =  DateTime.TryParse(evt?["start"]?.ToString(), null, DateTimeStyles.AssumeLocal, out var start);
 
     var isUpcoming = hasStart && start > DateTime.Now;
     var registerLink = evt?["registerLink"]?.ToString();
-
+    var startStr = start.ToString(Constants.EventDateFormat);
     var contentWithLayout = await WithLayout(title,
         Div(@class("p-4"),
             Div(style("display: flex; justify-content: center;"),
@@ -74,7 +75,7 @@ app.MapGet("/events/{eventSlug}", async (string eventSlug) => {
             ),
             H1(title),
             Hr(),
-            P("Start: ", start.ToString(Constants.EventDateFormat)),
+            P("Start: ", calendarUrl is not null ?  AHref(calendarUrl, startStr) : startStr),
             Hr(),
             RenderMarkdown(evt["description"].ToString()),
             If(isUpcoming && registerLink is not null, RegisterSection(registerLink)),
